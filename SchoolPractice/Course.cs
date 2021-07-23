d1cbeb3426dc5e54e4a57a635748257c2fd19f16
Megan Daubenmier <megan.daubenmier@gmail.com>
2021-07-22 20:01:05 -0500

    Exercises complete.

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    class Course
+    {
+        public string Instructor { get; set; }
+        public string CourseName { get; set; }
+        public List<Student> Students { get; set; }
+
+        public Course(string instructor, string name, List<Student> students)
+        {
+            Instructor = instructor;
+            CourseName = name;
+            Students = students;
+        }
+    }
+}
--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,7 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student megan = new Student("Megan Daubenmier", 001, 1, 4.0);
         }
     }
 }
