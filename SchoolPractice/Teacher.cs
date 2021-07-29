0dd6242a53010b248b8db43ce6f8e2d7c5559105
Megan Daubenmier <megan.daubenmier@gmail.com>
2021-07-28 19:07:01 -0500

    Further fixes based on in-class discussion.

--- SchoolPractice/Teacher.cs
+++ SchoolPractice/Teacher.cs
@@ -8,10 +8,10 @@ namespace SchoolPractice
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Subject { get; set; }
+        public Course Subject { get; set; }
         public int YearsTeaching { get; set; }
 
-        public Teacher(string firstName, string lastName, string subject, int numYears)
+        public Teacher(string firstName, string lastName, Course subject, int numYears)
         {
             this.FirstName = firstName;
             this.LastName = lastName;
